4013f4a9d170a2822b55fe61ec09464fef06cffa
Mjonesy1 <97568955+Mjonesy1@users.noreply.github.com>
2022-03-21 13:44:38 -0400

    Add files via upload

---
+++ lab9.cc
@@ -0,0 +1,31 @@
+//Lab9
+//Makenna Jones
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int dailyLow[30];
+int averageLow;
+int highestLow;
+int lowestLow;
+
+int main(){
+    ifstream instream; 
+    string filename = "athens_low_temps.txt"
+    instream.open (filename);
+    if instream (fail())
+        cout << "Error, can't open file" << endl;
+        exit(0);
+   
+    if instream (fail()){
+    for (int i = 0, i < 30, i++)    
+    instream >> dailyLow(){
+        }
+    }
+    lowest = lowestLow()
+    highest= highestLow()
+    average = averageLow()
+}
